b8e08617de32ba036b4d4f2f9452989c9d0d0c25
Ethan Hannum <ethanhannum10@gmail.com>
2026-08-15 13:29:06 -0700

    Add ability to create new basic Todos
    Added the ability to create new Todos and add them to a project, through
    the script. The todoCreation.createTodo method when called will save
    some variables, then use those to create a new Todo. This can easily be
    scaled once the DOM is up and running

--- src/index.js
+++ src/index.js
@@ -2,7 +2,35 @@ import "./styles.css";
 import { Project } from "./project.js";
 import { Todo } from "./todos.js";
 
-const testProject = new Project("test");
+const defaultProject = new Project("default");
 const testTodo = new Todo("test time", "1", "dueDate", "big description");
-testProject.addTodo(testTodo);
-console.log(testProject);
+defaultProject.addTodo(testTodo);
+
+const manageProjects = (() => {
+    const currentProjects = [defaultProject];
+})();
+
+const todoCreation = (() => {
+
+    function createTodo() {
+        const todoTitle = "test title";
+        const todoDescription = "test desc";
+        const todoDuedate = "test due date";
+        const todoPriority = "test priority";
+    
+        const newTodo = new Todo(todoTitle, todoPriority, todoDuedate, todoDescription);
+    
+        addToProject(newTodo, defaultProject);
+    }
+
+    const addToProject = (todo, project) => {
+        project.addTodo(todo);
+        console.log(defaultProject);
+    }
+
+    return { createTodo }
+})();
+
+
+
+todoCreation.createTodo();
