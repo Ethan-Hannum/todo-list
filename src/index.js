4722f11281c5be9fae0c60a8ad148baab7ef0684
Ethan Hannum <ethanhannum10@gmail.com>
2026-08-17 12:08:34 -0700

    Make tabs auto populate from currentProjects
    The tabs will be the main navigation for the app. Once a project tab is
    clicked, the todo's for that project should populate the main container

--- src/index.js
+++ src/index.js
@@ -1,36 +1,43 @@
 import "./styles.css";
-import { Project } from "./project.js";
-import { Todo } from "./todos.js";
+import { Project, Todo, manageProjects, todoCreation } from "./project.js";
 
-const defaultProject = new Project("default");
-const testTodo = new Todo("test time", "1", "dueDate", "big description");
-defaultProject.addTodo(testTodo);
+const tabContainer = document.querySelector("#tab-container");
+const mainContainer = document.querySelector("#main-container");
 
-const manageProjects = (() => {
-    const currentProjects = [defaultProject];
-})();
+const domLogic = (() => {
+    const addProjectToTab = () => {
+        const projectArr = manageProjects.checkProjects();
+        projectArr.forEach((item) => {
+            const tab = document.createElement("div");
+            tab.classList.add("tab");
+            tabContainer.appendChild(tab);
 
-const todoCreation = (() => {
-
-    function createTodo() {
-        const todoTitle = "test title";
-        const todoDescription = "test desc";
-        const todoDuedate = "test due date";
-        const todoPriority = "test priority";
-    
-        const newTodo = new Todo(todoTitle, todoPriority, todoDuedate, todoDescription);
-    
-        addToProject(newTodo, defaultProject);
+            const tabTitle = document.createElement("p");
+            tabTitle.textContent = item.title;
+            tab.appendChild(tabTitle);
+        }) 
     }
 
-    const addToProject = (todo, project) => {
-        project.addTodo(todo);
-        console.log(defaultProject);
-    }
+    const addTodos = () => {
+        const projectArr = manageProjects.checkProjects();
+        console.log(projectArr);
+        const projectTodoList = projectArr[0].todoList;
+        console.log(projectTodoList);
+        projectTodoList.forEach((todoItem) => {
+            const todoContainer = document.createElement("div");
+            const todoTitle = document.createElement("p");
+            todoContainer.classList.add("todo-container");
+            todoTitle.classList.add("todo-title");
+            todoTitle.textContent = todoItem.title;
+            
+            mainContainer.appendChild(todoContainer);
+            todoContainer.appendChild(todoTitle);  
+        })
 
-    return { createTodo }
+     
+    }
+    return {addProjectToTab, addTodos};
 })();
 
-
-
-todoCreation.createTodo();
+domLogic.addProjectToTab();
+domLogic.addTodos();
--- src/project.js
+++ src/project.js
@@ -1,4 +1,4 @@
-export { Project };
+export { Project, Todo, manageProjects, todoCreation };
 
 class Project {
     constructor(title) {
@@ -10,3 +10,49 @@ class Project {
         this.todoList.push(item);
     }
 }
+
+class Todo {
+    constructor(title, priority, dueDate, description) {
+        this.title = title;
+        this.priority = priority;
+        this.dueDate = dueDate;
+        this.description = description;
+        this.tabStatus = 0;
+    }
+}
+
+const manageProjects = (() => {
+    const currentProjects = [];
+
+    const checkProjects = () => currentProjects;
+
+    const createProject = (projectTitle) => {
+        const newProject = new Project(projectTitle);
+        currentProjects.push(newProject);
+    }
+
+    return { checkProjects, createProject };
+})();
+
+manageProjects.createProject("default");
+
+const todoCreation = (() => {
+    const addToProject = (todo, project) => {
+        project.addTodo(todo);
+    }
+
+    function createTodo() {
+        const projectArr = manageProjects.checkProjects();
+
+        const todoTitle = "test title";
+        const todoDescription = "test desc";
+        const todoDuedate = "test due date";
+        const todoPriority = "test priority";
+    
+        const newTodo = new Todo(todoTitle, todoPriority, todoDuedate, todoDescription);
+        addToProject(newTodo, projectArr[0]);
+    }
+    return { createTodo }
+})();
+
+todoCreation.createTodo();
